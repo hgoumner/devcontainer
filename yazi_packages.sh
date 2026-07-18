5674fc5fb630c24bb0c72e47116d1e25a047d431
JF82788 <hristo.goumnerov@kbc.com>
2026-07-18 10:42:26 +0300

    feat(yazi): add plugin collection

---
+++ yazi_packages.sh
@@ -0,0 +1,23 @@
+#!/bin/env zsh
+
+packages=(
+  "AnirudhG07/plugins-yazi:copy-file-contents"
+  "AnirudhG07/rich-preview"
+  "Ape/simple-status"
+  "h-hg/yamb"
+  "malick-tammal/monokai"
+  "wylie102/duckdb"
+  "yazi-rs/plugins:diff"
+  "yazi-rs/plugins:git"
+  "yazi-rs/plugins:jump-to-char"
+  "yazi-rs/plugins:smart-enter"
+  "yazi-rs/plugins:smart-filter"
+  "yazi-rs/plugins:toggle-pane"
+  "yazi-rs/plugins:types"
+  "yazi-rs/plugins:vcs-files"
+)
+
+for pkg in "${packages[@]}"; do
+  echo "Installing $pkg..."
+  ya pkg add "$pkg"
+done
